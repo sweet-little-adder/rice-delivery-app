d35ac5786c55cc1f1f09fac7e1c36fe4351d5360
adder <59740233+sweet-little-adder@users.noreply.github.com>
2022-03-31 22:03:13 +0800

    initial styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
+import Header from "./components/Header";
+import Button from "./components/Button";
+import "./App.css";
 
-const App = () => <div>Rice Delivery App</div>;
+const App = () => (
+  <div>
+    <Header>Rice Delivery App</Header>
+    <Button>Deliver</Button>
+  </div>
+);
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import styled from "./node_modules/styled-components";
+
+export default styled.h1`
+  font-family: ${(props) => props.theme.font};
+`;
